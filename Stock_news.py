0f8eb448e9b11ec3a2573810512b4fe68afb6717
Miroslav Kopecky <kopecky.mir@gmail.com>
2023-02-19 17:37:06 +0100

    parse summary and tickers and writes a txt

--- Stock_news.py
+++ Stock_news.py
@@ -14,12 +14,13 @@ sorted_data = (json.dumps(data, indent=4, sort_keys=True))
 # with open('test_data.txt', 'w') as file:
 #     file.writelines(sorted_data)
 
-for items in data['feed']:
-    for ticker in items['ticker_sentiment']:
-        tickers = (ticker['ticker'])
-    summary = (items['summary'])
-    
-    print(tickers, end='\n')
-    print(summary)
+with open('test_data.txt', 'w') as file:
+
+    for items in data['feed']:
+        for ticker in items['ticker_sentiment']:
+            tickers = (ticker['ticker'])
+        summary = (items['summary'])
+        
+        file.write(str(tickers) + ':' + ' ' + str(summary) + '\n')
 
 
