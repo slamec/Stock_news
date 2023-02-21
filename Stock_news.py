af146d0e23ffc3bccfbb69bc70ac67500df55997
Miroslav Kopecky <kopecky.mir@gmail.com>
2023-02-21 22:07:32 +0100

    parses ticker, headline and url

--- Stock_news.py
+++ Stock_news.py
@@ -4,23 +4,25 @@ import json
 with open('API_key.txt') as key:
     api_key = key.read()
 
-url = 'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&apikey=api_key'
+url = 'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=PG&sort=LATEST&limit=5&apikey=api_key'
 r = requests.get(url)
 data = r.json()
 
 sorted_data = (json.dumps(data, indent=4, sort_keys=True))
 
 
-# with open('test_data.txt', 'w') as file:
-#     file.writelines(sorted_data)
+with open('dict.txt', 'w') as file:
+    file.writelines(sorted_data)
 
 with open('test_data.txt', 'w') as file:
 
     for items in data['feed']:
         for ticker in items['ticker_sentiment']:
             tickers = (ticker['ticker'])
-        summary = (items['summary'])
+        summary = items['summary']
+        links = items['url']
+    
         
-        file.write(str(tickers) + ':' + ' ' + str(summary) + '\n')
+        file.write(str(tickers) + ':' + '\n' + str(summary) + '\n' + str(links) + '\n')
 
 
