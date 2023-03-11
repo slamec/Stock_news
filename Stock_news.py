b6306d6bd6d01600cc5591dcaa907f08fdae4e2d
Miroslav Kopecky <kopecky.mir@gmail.com>
2023-03-11 13:30:14 +0100

    print news and avoid duplicates

--- Stock_news.py
+++ Stock_news.py
@@ -1,13 +1,32 @@
 import yfinance as yf 
 import json
 
+symbol = 'AAPL'
 
-ticker = yf.Ticker('AAPL')
+ticker = yf.Ticker(symbol)
 ticker_news = ticker.news
 
 data_sorted = json.dumps(ticker_news, indent=4, sort_keys=True)
 
-# print(data_sorted)
+news_dict = {}
 
-with open('test_data.txt', 'w') as file:
-    file.writelines(data_sorted)
+for items in ticker_news:
+
+    related_ticker = items['relatedTickers']
+    link = items['link']
+    title = items['title']
+
+    for ticker in related_ticker:
+        if ticker not in news_dict:
+            news_dict[ticker] = set()
+        else:
+            news_dict[ticker].add((title, link))
+
+for title, link in news_dict.get(symbol, []):
+    print(symbol)
+    print(title)
+    print(link)
+
+
+
+
