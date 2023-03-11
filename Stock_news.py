284aea3d02ae21187b0eb90677a6b92f6920985d
Miroslav Kopecky <kopecky.mir@gmail.com>
2023-03-11 13:44:37 +0100

    multiple stocks news

--- Stock_news.py
+++ Stock_news.py
@@ -1,31 +1,38 @@
 import yfinance as yf 
 import json
 
-symbol = 'AAPL'
+symbols = ['AAPL', 'GOOG', 'AMZN']
 
-ticker = yf.Ticker(symbol)
-ticker_news = ticker.news
+for symbol in symbols:
+    ticker = yf.Ticker(symbol)
+    ticker_news = ticker.news
 
-data_sorted = json.dumps(ticker_news, indent=4, sort_keys=True)
+    data_sorted = json.dumps(ticker_news, indent=4, sort_keys=True)
 
-news_dict = {}
+    news_dict = {}
 
-for items in ticker_news:
+    for items in ticker_news:
+        related_ticker = items['relatedTickers']
+        title = items['title']
+        link = items['link']
+        
 
-    related_ticker = items['relatedTickers']
-    link = items['link']
-    title = items['title']
-
-    for ticker in related_ticker:
-        if ticker not in news_dict:
-            news_dict[ticker] = set()
-        else:
+        for ticker in related_ticker:
+            if ticker not in news_dict:
+                news_dict[ticker] = set()
             news_dict[ticker].add((title, link))
 
-for title, link in news_dict.get(symbol, []):
-    print(symbol)
-    print(title)
-    print(link)
+    count = 0
+
+    for title, link in news_dict.get(symbol, []):
+        print(symbol)
+        print(title)
+        print(link)
+        count += 1
+        if count == 3:
+            break
+
+    print('\n')
 
 
 
