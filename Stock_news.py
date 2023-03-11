35b2baf90c78551c9a932849fe57f79677946b0a
Miroslav Kopecky <kopecky.mir@gmail.com>
2023-03-11 11:26:28 +0100

    first commit with yfinance

--- Stock_news.py
+++ Stock_news.py
@@ -1,31 +1,13 @@
-import requests 
+import yfinance as yf 
 import json
 
-with open('API_key.txt') as key:
-    api_key = key.read()
 
-tickers = ['AAPL', 'MSFT', 'GOOGL']
+ticker = yf.Ticker('AAPL')
+ticker_news = ticker.news
 
-for ticker in tickers:
+data_sorted = json.dumps(ticker_news, indent=4, sort_keys=True)
 
-    url = f'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&sort=LATEST&limit=10&apikey={api_key}'
-
-    r = requests.get(url)
-    data = r.json()
-
-    sorted_data = (json.dumps(data, indent=4, sort_keys=True))
-
-
-with open('dict.txt', 'w') as file:
-    file.writelines(sorted_data)
+# print(data_sorted)
 
 with open('test_data.txt', 'w') as file:
-    for items in data['feed']:
-        for ticker in items['ticker_sentiment']:
-            tickers = (ticker['ticker'])
-        summary = items['summary']
-        links = items['url']
-    
-        
-        file.write(str(tickers) + ':' + '\n' + str(summary) + '\n' + str(links) + '\n')
-
+    file.writelines(data_sorted)
