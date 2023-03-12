d19ac689bdece323f9d84056584ee3e4a0204a7d
Miroslav Kopecky <kopecky.mir@gmail.com>
2023-03-12 21:19:37 +0100

    writes output to a filed

--- Stock_news.py
+++ Stock_news.py
@@ -19,48 +19,55 @@ symbols = read_csv(portfolio)
 
 #print(symbols)
 
-def ticker_news(stock_name):
+import yfinance as yf
+
+def ticker_news(stock_name, output_file):
     """This function accepts n number of ticker symbols and returns 3 top news for each."""
-
-    #get news for ticker
-    for symbol in stock_name:
-        ticker = yf.Ticker(symbol)
-        ticker_news = ticker.news
-        
-        #new dictionary 
-        news_dict = {}
-        
-        #keep track of news items that have already been added
-        added_news = set()
-
-        #loop through yf dictionary
-        for items in ticker_news:
-            try:
-                related_ticker = items['relatedTickers']
-            except KeyError:
-                related_ticker = []
-            title = items['title']
-            link = items['link']
+    
+    # Open the file for writing
+    with open(output_file, 'w') as f:
+    
+        # Get news for each ticker
+        for symbol in stock_name:
+            ticker = yf.Ticker(symbol)
+            ticker_news = ticker.news
             
-            #avoid duplicates
-            if (title, link) not in added_news:
-                for ticker in related_ticker:
-                    if ticker not in news_dict:
-                        news_dict[ticker] = set()
-                    news_dict[ticker].add((title, link))
-                added_news.add((title, link))
+            # New dictionary
+            news_dict = {}
+            
+            # Keep track of news items that have already been added
+            added_news = set()
+            
+            # Loop through yf dictionary
+            for items in ticker_news:
+                try:
+                    related_ticker = items['relatedTickers']
+                except KeyError:
+                    related_ticker = []
+                title = items['title']
+                link = items['link']
+                
+                # Avoid duplicates
+                if (title, link) not in added_news:
+                    for ticker in related_ticker:
+                        if ticker not in news_dict:
+                            news_dict[ticker] = set()
+                        news_dict[ticker].add((title, link))
+                    added_news.add((title, link))
+            
+            count = 0
+            
+            # Write news to file
+            for title, link in news_dict.get(symbol, []):
+                f.write(f"{symbol}\n")
+                f.write(f"{title}\n")
+                f.write(f"{link}\n")
+                count += 1
+                if count == 3:
+                    break
+            
+            f.write('\n')
 
-        count = 0
+output_file = 'news_test.txt'
 
-        #print news from python 
-        for title, link in news_dict.get(symbol, []):
-            print(symbol)
-            print(title)
-            print(link)
-            count += 1
-            if count == 3:
-                break
-
-        print('\n')
-
-ticker_news(symbols)
+ticker_news(symbols, output_file)
