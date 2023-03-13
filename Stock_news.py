40680a0a7534c401f9039f90b038c85a341522b8
Miroslav Kopecky <kopecky.mir@gmail.com>
2023-03-13 22:01:36 +0100

    return added to ticker_news

--- Stock_news.py
+++ Stock_news.py
@@ -21,53 +21,52 @@ symbols = read_csv(portfolio)
 
 import yfinance as yf
 
-def ticker_news(stock_name, output_file):
+def ticker_news(stock_name):
     """This function accepts n number of ticker symbols and returns 3 top news for each."""
     
-    # Open the file for writing
-    with open(output_file, 'w') as f:
-    
-        # Get news for each ticker
-        for symbol in stock_name:
-            ticker = yf.Ticker(symbol)
-            ticker_news = ticker.news
-            
-            # New dictionary
-            news_dict = {}
-            
-            # Keep track of news items that have already been added
-            added_news = set()
-            
-            # Loop through yf dictionary
-            for items in ticker_news:
-                try:
-                    related_ticker = items['relatedTickers']
-                except KeyError:
-                    related_ticker = []
-                title = items['title']
-                link = items['link']
-                
-                # Avoid duplicates
-                if (title, link) not in added_news:
-                    for ticker in related_ticker:
-                        if ticker not in news_dict:
-                            news_dict[ticker] = set()
-                        news_dict[ticker].add((title, link))
-                    added_news.add((title, link))
-            
-            count = 0
-            
-            # Write news to file
-            for title, link in news_dict.get(symbol, []):
-                f.write(f"\n{symbol}\n")
-                f.write('\n')
-                f.write(f"<a href=\"{link}\">{title}</a>\n")
-                count += 1
-                if count == 3:
-                    break
-            
-            f.write('<br><br>\n')
+    news_string = ""
 
-output_file = 'news_test.html'
+    # Get news for each ticker
+    for symbol in stock_name:
+        ticker = yf.Ticker(symbol)
+        ticker_news = ticker.news
+        
+        # New dictionary
+        news_dict = {}
+        
+        # Keep track of news items that have already been added
+        added_news = set()
+        
+        # Loop through yf dictionary
+        for items in ticker_news:
+            try:
+                related_ticker = items['relatedTickers']
+            except KeyError:
+                related_ticker = []
+            title = items['title']
+            link = items['link']
+            
+            # Avoid duplicates
+            if (title, link) not in added_news:
+                for ticker in related_ticker:
+                    if ticker not in news_dict:
+                        news_dict[ticker] = set()
+                    news_dict[ticker].add((title, link))
+                added_news.add((title, link))
+        
+        count = 0
+        
+        # Write news to file
+        for title, link in news_dict.get(symbol, []):
+            news_string += '\n'
+            news_string += f"{symbol}"
+            news_string += f"\n<a href=\"{link}\">{title}</a>\n"
+            count += 1
+            if count == 3:
+                break
+        
+        
+    return news_string
 
-ticker_news(symbols, output_file)
+
+print(ticker_news(symbols))
